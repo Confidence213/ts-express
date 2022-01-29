231ea555eb1a4133ae60e834b8a6e3faa9ff4884
jackcolin21307 <jackcolin21307@gmail.com>
2022-01-29 21:18:18 +0200

    Updated packages

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ dotenv.config({ path: ".env" });
 const app: express.Application = express();
 
 const mongoUrl = MONGODB_URI;
-mongoose.connect(mongoUrl, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true }, (err: any) => {
+mongoose.connect(mongoUrl, (err: any) => {
     if (err) {
         // tslint:disable-next-line:no-console
         console.log(err.message);
@@ -26,10 +26,6 @@ mongoose.connect(mongoUrl, { useCreateIndex: true, useNewUrlParser: true, useUni
 app.set("port", process.env.PORT || 3000);
 
 applyMiddleware(middleware, app);
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
 
 app.post("/", (req, res) => {
     res.send(req.body);
--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -5,10 +5,8 @@ import logger from "./logger";
 if (fs.existsSync(".env")) {
     logger.debug("Using .env file to supply config environment variables");
     dotenv.config({ path: ".env" });
-} else {
-    logger.debug("Using .env.example file to supply config environment variables");
-    dotenv.config({ path: ".env.example" });  // you can delete this after you create your own .env file!
 }
+
 export const ENVIRONMENT = process.env.NODE_ENV;
 const prod = ENVIRONMENT === "production";
 
